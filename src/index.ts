c27b488b44b4f606d01ee5bf367319231beb49cf
Carlos Ratia <carlos.ratia@bunkerdb.com>
2021-01-26 13:56:29 +0100

    [NPM] - MW Level APP

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,77 @@
-import express, {Application, NextFunction, Request, Response} from 'express';
-import createHttpError from "http-errors";
-
-const sum = (a: number, b: number): number => {
-  return a + b;
-};
+import express, { Application, NextFunction, Request, Response } from 'express';
+import createHttpError from 'http-errors';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import helmet from 'helmet';
 
 //npm install express
 //npm install -D @types/express
 const app: Application = express();
 const port: number = 5000;
 
-//MW
-// app.use(HANDLE)
-// HANDLER: (req:Request, resp:Response, next: NextFunction): void
-// MW -> APLICACION
-//  Para pasar objetos a traves de MW se puede realizar en la varialer res.locals
+//npm install body-parse
+//npm install -D @types/body-parse
+//npm ERR! code E404
+// npm ERR! 404 Not Found - GET https://registry.npmjs.org/@types%2fbody-parse - Not found
+// npm ERR! 404
+// npm ERR! 404  '@types/body-parse@latest' is not in the npm registry.
+// npm ERR! 404 You should bug the author to publish it (or use the name yourself!)
+// npm ERR! 404
+// npm ERR! 404 Note that you can also install from a
+// npm ERR! 404 tarball, folder, http url, or git url.
 //
-app.use((_req:Request, res:Response, next: NextFunction)=> {
-  res.locals.a = 1
-  next();
-})
+// npm ERR! A complete log of this run can be found in:
+// npm ERR!     /Users/bunker/.npm/_logs/2021-01-26T12_29_00_088Z-debug.log
+// OJO: No es necesario instalar los types por que el paquete body-parser ya los contiene.
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((_req:Request, res:Response, next: NextFunction)=> {
-  res.locals.b = 2
-  next();
-})
+//npm install cors
+//npm install -D @types/cors
+app.use(cors({ origin: /^https:\/\/(.*)\.(bunkerdb|eagle-latam)\.com$/ }));
 
-app.use((_req:Request, res:Response, next: NextFunction)=> {
-  console.log('MW');
-  console.log(res.locals);
-  next();
-})
+app.use(helmet());
 
+//TODO: Manrdar archivos.
+//
 // |
 // |
 // v
 // app.[get|post|put|delete...|all](PATTERN, HANDLE);
 // app.get('/ping', (req, res) => {});
 // src/index.ts:7:19 - error TS6133: 'req' is declared but its value is never read.
-//  Para evitar este tipo de error en tiempo de compilacion se le agrega una _ adelanta de la variable
+//  Para evitar este tipo de error en tiempo de compilacion se le agrega una _ adelante de la variable
 app.get('/ping', (_req, res) => {
   console.log('ROUTE');
   console.log(res.locals);
-  res.json({
+  res.status(200).json({
     statusCode: 200,
     data: { status: 'ok' },
   });
 });
 
-app.all('/pong', (req: Request, res: Response) => {
-  console.log(req.params);
-  res.json({
-    statusCode: 200,
-    data: { status: 'ok' },
-  });
-})
+app.post(
+  '/test',
+  (_req, _res, next) => {
+    console.log('ME LLAMARON');
+    //LOGICA
+    next();
+  },
+  (req, res) => {
+    console.log(req.body);
+    console.log(req.query);
+    res.status(200).json({
+      statusCode: 200,
+      data: req.body,
+    });
+  }
+);
 
-//ROUTER -> Dependencia ????
-app.get('/:a(\\d+)/:b(\\d+)/sum', (req, res) => {
-  const params = req.params;
-  console.log(req.query);
-  let a: string | undefined = params.a;
-  let b: string | undefined = params.b;
-  let aNumber: number = a === undefined ? 0 : parseFloat(a);
-  let bNumber: number = b === undefined ? 0 : parseFloat(b);
-
-  res.json({
-    statusCode: 200,
-    data: sum(aNumber, bNumber),
-  });
-});
-
-app.use((_req: Request, _res: Response, next:NextFunction) => {
+app.use((_req: Request, _res: Response, next: NextFunction) => {
   //MW NO FOUNY
-  next(createHttpError(404))
+  next(createHttpError(404));
 });
-
 
 app.listen(port, () => {
   console.log(
