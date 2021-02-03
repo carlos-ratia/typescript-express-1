d8e4bd3c79170653548132dc8068156fe894c249
Carlos Ratia <carlos.ratia@bunkerdb.com>
2021-02-03 14:17:06 +0100

    [Migrations] Intro to Sequelize

--- migrations/20210203125630-AddModelCampaign.js
+++ migrations/20210203125630-AddModelCampaign.js
@@ -2,34 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface
-      .createTable(
-        "Campaign",
-        {
-          id: {
-            type: Sequelize.STRING,
-            primaryKey: true,
-          },
-          name: {
-            type: Sequelize.STRING,
-            allowNull: false,
-          },
-          brandId: {
-            type: Sequelize.STRING(40),
-            allowNull: false,
-            //TODO: COMO REALIZARLO
-            // references: 'Brand', // <<< Note, its table's name, not object name
-            // referencesKey: 'id' // <<< Note, its a column name
-          },
+    await queryInterface.createTable(
+      "Campaign",
+      {
+        id: {
+          type: Sequelize.STRING,
+          primaryKey: true,
         },
-        {
-          charset: "utf8mb4",
-          collate: "utf8mb4_general_ci",
-        }
-      );
-        await queryInterface.sequelize.query(
-          "ALTER TABLE `Campaign` ADD CONSTRAINT `fk_campaing_brand` FOREIGN KEY (`brandId`) REFERENCES `Brand` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;"
-        );
+        name: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        brandId: {
+          type: Sequelize.STRING(40),
+          allowNull: false,
+          //TODO: COMO REALIZARLO
+          // references: 'Brand', // <<< Note, its table's name, not object name
+          // referencesKey: 'id' // <<< Note, its a column name
+        },
+      },
+      {
+        charset: "utf8mb4",
+        collate: "utf8mb4_general_ci",
+      }
+    );
+    await queryInterface.sequelize.query(
+      "ALTER TABLE `Campaign` ADD CONSTRAINT `fk_campaing_brand` FOREIGN KEY (`brandId`) REFERENCES `Brand` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;"
+    );
   },
 
   down: async (queryInterface, Sequelize) => {
